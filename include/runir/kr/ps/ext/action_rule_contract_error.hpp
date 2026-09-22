9ac39aa3ec6c5e58aeca67dfe48da6a034e8bb2a
Dominik Drexler <drexlerd93@gmail.com>
2026-09-23 01:14:13 +0200

    fix

--- include/runir/kr/ps/ext/action_rule_contract_error.hpp
+++ include/runir/kr/ps/ext/action_rule_contract_error.hpp
@@ -3,15 +3,24 @@
 
 #include <stdexcept>
 
+#if defined(__clang__) || defined(__GNUC__)
+#define RUNIR_KR_PS_EXT_ERROR_API __attribute__((visibility("default")))
+#else
+#define RUNIR_KR_PS_EXT_ERROR_API
+#endif
+
 namespace runir::kr::ps::ext
 {
 
-class ActionRuleContractError : public std::runtime_error
+// The native library and Python exception translator must share this type's RTTI.
+class RUNIR_KR_PS_EXT_ERROR_API ActionRuleContractError : public std::runtime_error
 {
 public:
     using std::runtime_error::runtime_error;
 };
 
 }  // namespace runir::kr::ps::ext
+
+#undef RUNIR_KR_PS_EXT_ERROR_API
 
 #endif
